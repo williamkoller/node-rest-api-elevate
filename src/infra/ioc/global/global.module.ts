97e5c4d7410df293aa1b923c93be9a6938b375eb
William Koller <williamkoller30@gmail.com>
2024-10-03 16:25:50 -0300

    feat: add variavel path env

--- src/infra/ioc/global/global.module.ts
+++ src/infra/ioc/global/global.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import globalConf, { GlobalConfig } from '../../config/global/global.config';
 
-const pathEnv = process.env.PATH_ENV;
+const pathEnv = process.env.PATH_ENV || '.env';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: pathEnv ?? '.env.development.local',
+      envFilePath: pathEnv,
+      load: [globalConf],
     }),
   ],
+  providers: [GlobalConfig],
 })
 export class GlobalModule {}
