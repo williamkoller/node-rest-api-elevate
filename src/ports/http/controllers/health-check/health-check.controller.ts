8c1b410f2ee89b84a0284bee54eeddb6d8a9c0af
William Koller <williamkoller30@gmail.com>
2024-10-03 16:26:09 -0300

    refactor: add return string

--- src/ports/http/controllers/health-check/health-check.controller.ts
+++ src/ports/http/controllers/health-check/health-check.controller.ts
@@ -1,11 +1,8 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { SentryInterceptor } from '../../interceptors/sentry.interceptor';
-import { CustomSentryInterceptor } from '../../interceptors/custom-sentry.interceptor';
 
 @ApiTags('health-check')
 @Controller('health-check')
-@UseInterceptors(SentryInterceptor, CustomSentryInterceptor)
 export class HealthCheckController {
   @Get()
   @ApiOperation({
@@ -22,7 +19,6 @@ export class HealthCheckController {
     },
   })
   healthCheck() {
-    // return 'Health check is OK!';
-    throw new Error('Health check is not OK!');
+    return 'Health check is OK!';
   }
 }
