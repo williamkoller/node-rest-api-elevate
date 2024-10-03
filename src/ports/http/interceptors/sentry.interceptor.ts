924a48a2457fba2ffa614c38a2ffdace34847d98
William Koller <williamkoller30@gmail.com>
2024-10-03 16:24:57 -0300

    fea: add captureException in sentry interceptor

--- src/ports/http/interceptors/sentry.interceptor.ts
+++ src/ports/http/interceptors/sentry.interceptor.ts
@@ -1,19 +1,21 @@
 import {
+  ExecutionContext,
   Injectable,
   NestInterceptor,
-  ExecutionContext,
   CallHandler,
   HttpException,
 } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import * as Sentry from '@sentry/node';
 
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError((error) => {
+        Sentry.captureException(error);
+
         if (error instanceof HttpException) {
           const response = error.getResponse();
           const responseBody =
